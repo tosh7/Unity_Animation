ef50ebc9488d007eac30d8630caf71799e8158e5
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-12-23 17:05:22 +0900

    first init

---
+++ Assets/Locomotion Setup/Locomotion/PlayerAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animator = GetComponent<Animator> ();
+
+		if(Input.GetKey (KeyCode.Space)){
+			animator.SetBool("Jump", true);
+		} else {
+			animator.SetBool("Jump", false);
+		}
+	}
+}
